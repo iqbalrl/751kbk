03ece02dca2247efe923caa0276c09495350a887
iqbalrl <ibaalrl15@yahoo.com>
2018-11-09 04:29:26 +0700

    hmmmm20jam

--- cfg.php
+++ cfg.php
@@ -1,14 +1,12 @@
 <?php
-$android_id = "6e462d1326d8c0c1";
-$deviceCode = "865723037335651";
-$device_ip  = "203.78.119.218";
-$token = 
-"731fQpwyTL8r8PU_oK26KZS8XUUtBRwSN9ZjdCN6iHFsTQyGAY52F8DmZxqRLDd0F4QZsHkLFuoaiJA";
-$tk = 
-"ACH7Tsxzr4tKfoUvkAc4HoiKEz_qEDH3KgBxdHRodw";
-$uuid = "4b35fc28c1f14e71a82ba70926da5e9b";
-$userid = "2445954";
-$sign = "d31bb68b5b54a70174be36cadfea1d2a";
+$android_id = "6e462d1XXXXX";
+$deviceCode = "86572XXXXX";
+$device_ip  = "203.78.XXX.XX.XX";
+$token = "731fQpwXXXXX";
+$tk = "ACH7TsxzXXXXXX";
+$uuid = "4b35fc2XXXXX";
+$userid = "24XXXXX";
+$sign = "d31XXXXXXXXXXX";
 
 ####################################################
 #|note:
